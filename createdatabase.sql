0aa223e8211164702db98632b233987b9b65b4e9
Chantelle Robinson <chantellelouiserobinson92@gmail.com>
2020-01-30 10:45:13 +0000

    Revert "Update createdatabase.sql"
    This reverts commit 2ae45e6bb47edbcc55eb3781109bfd1fb20ad748.

--- createdatabase.sql
+++ createdatabase.sql
@@ -1,5 +1,5 @@
 CREATE TABLE community_orgs (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY,
     community_org_name VARCHAR(40) UNIQUE NOT NULL,
     telephone VARCHAR(15),
     email VARCHAR(50) UNIQUE,
@@ -9,18 +9,11 @@ CREATE TABLE community_orgs (
 );
 
 CREATE TABLE categories (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY,
     category_name VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE community_org_categories (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    CONSTRAINT `community_org_constraint` 
-        FOREIGN KEY(community_orgs_id) REFERENCES community_orgs(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE, 
-    CONSTRAINT `categories_constraint` 
-        FOREIGN KEY(categories_id) REFERENCES categories(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
+    community_org_id INT,
+    categories_id INT
 );
