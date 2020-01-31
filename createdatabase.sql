6e928d309a3f75d6c78732440447a19a99f84c9a
Chantelle Robinson <chantellelouiserobinson92@gmail.com>
2020-01-31 14:28:21 +0000

    Update createdatabase.sql

--- createdatabase.sql
+++ createdatabase.sql
@@ -1,18 +1,22 @@
+CREATE DATABASE  IF NOT EXISTS community-org-finder;
+USE community-org-finder;
+
+DROP TABLE IF EXISTS community_orgs;
 CREATE TABLE community_orgs (
     id INT PRIMARY KEY AUTO_INCREMENT,
     community_org_name VARCHAR(40) UNIQUE NOT NULL,
     telephone VARCHAR(15),
     email VARCHAR(50) UNIQUE,
-    address VARCHAR(70),
-    longitude DECIMAL,
-    latitude DECIMAL
+    address
 );
 
+DROP TABLE IF EXISTS categories;
 CREATE TABLE categories (
     id INT PRIMARY KEY AUTO_INCREMENT,
     category_name VARCHAR(50) UNIQUE NOT NULL
 );
 
+DROP TABLE IF EXISTS community_org_categories;
 CREATE TABLE community_org_categories (
     id INT PRIMARY KEY AUTO_INCREMENT,
     CONSTRAINT `community_org_constraint` 
