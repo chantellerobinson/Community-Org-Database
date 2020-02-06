eca9b71601e6af696cf1bc5a50ae40ca06441e9f
Chantelle Robinson <chantellelouiserobinson92@gmail.com>
2020-02-06 15:28:46 +0000

    Update createdatabase.sql

--- createdatabase.sql
+++ createdatabase.sql
@@ -3,7 +3,7 @@ USE `community_org_finder`;
 
 DROP TABLE IF EXISTS `community_orgs`;
 CREATE TABLE `community_orgs` (
-    community_orgs_id INT PRIMARY KEY AUTO_INCREMENT,
+    community_orgs_id INT(4) PRIMARY KEY AUTO_INCREMENT,
     community_org_name VARCHAR(40) UNIQUE NOT NULL,
     telephone VARCHAR(15),
     email VARCHAR(50) UNIQUE,
@@ -14,15 +14,15 @@ CREATE TABLE `community_orgs` (
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
-    categories_id INT PRIMARY KEY AUTO_INCREMENT,
+    categories_id INT(4) PRIMARY KEY AUTO_INCREMENT,
     category_name VARCHAR(50) UNIQUE NOT NULL
 );
 
 DROP TABLE IF EXISTS `community_org_categories`;
 CREATE TABLE `community_org_categories` (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    community_orgs_id  INT NOT NULL,
-    categories_id INT NOT NULL,
+    id INT(4) PRIMARY KEY AUTO_INCREMENT,
+    community_orgs_id  INT(4) NOT NULL,
+    categories_id INT(4) NOT NULL,
         FOREIGN KEY(community_orgs_id) REFERENCES community_orgs(id)
         ON UPDATE CASCADE
         ON DELETE CASCADE,  
