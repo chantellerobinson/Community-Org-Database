c77a9207b9a016a4cb49c417521cc97acecaeb2a
Chantelle Robinson <chantellelouiserobinson92@gmail.com>
2020-02-06 15:38:14 +0000

    Update createdatabase.sql

--- createdatabase.sql
+++ createdatabase.sql
@@ -3,7 +3,7 @@ USE `community_org_finder`;
 
 DROP TABLE IF EXISTS `community_orgs`;
 CREATE TABLE `community_orgs` (
-    community_orgs_id INT(4) PRIMARY KEY AUTO_INCREMENT,
+    id INT(4) PRIMARY KEY AUTO_INCREMENT,
     community_org_name VARCHAR(40) UNIQUE NOT NULL,
     telephone VARCHAR(15),
     email VARCHAR(50) UNIQUE,
@@ -14,7 +14,7 @@ CREATE TABLE `community_orgs` (
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
-    categories_id INT(4) PRIMARY KEY AUTO_INCREMENT,
+    id INT(4) PRIMARY KEY AUTO_INCREMENT,
     category_name VARCHAR(50) UNIQUE NOT NULL
 );
 
